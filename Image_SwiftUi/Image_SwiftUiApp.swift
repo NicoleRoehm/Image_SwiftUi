dca48f64c6b4679559e2627ae7bae6fd6085059f
NicoleRöhm <donjay1014@gmail.com>
2023-05-08 12:08:23 +0200

    Initial Commit

---
+++ Image_SwiftUi/Image_SwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_SwiftUiApp.swift
+//  Image_SwiftUi
+//
+//  Created by Nicole Röhm on 08.05.23.
+//
+
+import SwiftUI
+
+@main
+struct Image_SwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
